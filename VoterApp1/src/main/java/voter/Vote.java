47c5171f4110756be225bfa07e63cb530730188b
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-09-25 12:12:32 +0530

    voter app check age is greater than 18 or not

---
+++ VoterApp1/src/main/java/voter/Vote.java
@@ -0,0 +1,53 @@
+package voter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Vote
+ */
+@WebServlet("/vote")
+public class Vote extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Vote() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//request generate 
+		String age=request.getParameter("age");
+		PrintWriter out=response.getWriter();
+		out.println(age);
+		//check voter id 
+		int age1=Integer.parseInt(age);
+		if(age1>18) {
+		out.println("valid");	
+		}
+		else {
+			out.println("not valid ");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
